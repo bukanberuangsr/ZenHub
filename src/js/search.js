cd23129a9f05f0bf45f8ebb5486ec1a7ad068143
Akhtar Hafiz P <akhfizpuli@gmail.com>
2024-08-27 16:24:02 +0700

    Search Engine Change: Brave to DuckDuckGo

--- src/js/search.js
+++ src/js/search.js
@@ -1,6 +1,6 @@
 function performSearch(event) {
     event.preventDefault();
     const query = document.getElementById("search-input").value;
-    const searchurl = "https://search.brave.com/search?q=" +encodeURIComponent(query);
+    const searchurl = "https://duckduckgo.com/?t=h_&q=" +encodeURIComponent(query);
     window.location.href = searchurl;
 }
